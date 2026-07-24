cf41f39e358c42c59f7bdd335a5be2db40a76531
Gnandev02 <gnandevbonthu12@gmail.com>
2026-07-24 07:10:07 +0530

    Update Firebase configuration credentials to new project

--- internarea/src/firebase/firebase.js
+++ internarea/src/firebase/firebase.js
@@ -1,20 +1,22 @@
 // Import the functions you need from the SDKs you need
 import { initializeApp } from "firebase/app";
-// TODO: Add SDKs for Firebase products that you want to use
-// https://firebase.google.com/docs/web/setup#available-libraries
 import { getAuth, GoogleAuthProvider } from "firebase/auth";
+
 // Your web app's Firebase configuration
 const firebaseConfig = {
-  apiKey: "AIzaSyDbYTYs_oSK2rvtzHxBCPFrqdXM-l7MokQ",
-  authDomain: "internshala-9e300.firebaseapp.com",
-  projectId: "internshala-9e300",
-  storageBucket: "internshala-9e300.firebasestorage.app",
-  messagingSenderId: "304425115290",
-  appId: "1:304425115290:web:1b0653f1acd4192b848923",
+  apiKey: "AIzaSyBO-Ugc8KOXN3fjwy3aqwQtOgSqxRARlys",
+  authDomain: "internshala-clone-b55c5.firebaseapp.com",
+  projectId: "internshala-clone-b55c5",
+  storageBucket: "internshala-clone-b55c5.firebasestorage.app",
+  messagingSenderId: "585926719131",
+  appId: "1:585926719131:web:52f5f4e973d7a81d38383e",
+  measurementId: "G-QNHF2FHK48"
 };
 
 // Initialize Firebase
 const app = initializeApp(firebaseConfig);
 const auth = getAuth(app);
 const provider = new GoogleAuthProvider();
+
 export { auth, provider };
+
